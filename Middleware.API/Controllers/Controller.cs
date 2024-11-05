d2e289fa13bf68a20623f7a98aa67fd85738974e
Romain <romain.reydel@gmail.com>
2024-11-05 16:28:58 +0100

    init

---
+++ Middleware.API/Controllers/Controller.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.API.Controllers
+{
+    public class ActionInput
+    {
+        public string Key { get; set; } = string.Empty;
+        public object Input { get; set; } = new();
+    }    
+
+
+    [ApiController]
+    [Route("/")]
+    public class Controller : ControllerBase
+    {
+        private readonly ILogger<Controller> _logger;
+
+        public Controller(ILogger<Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            var t = Request.HttpContext.Connection.RemoteIpAddress;
+            return Ok(new { IP = t });
+        }
+
+        [HttpPost("action")]
+        public IActionResult Action([FromBody] ActionInput input)
+        {
+            return Ok(input);
+        }
+
+        [HttpGet("my_swag")]
+        public IActionResult MySwag()
+        {
+            return Ok("<html><h1>Hello Paul</h1></html>");
+        }
+    }
+}
