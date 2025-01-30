66232008caf6090a7949a3ccec4a7c90a60c12d1
Romain <romain.reydel@gmail.com>
2025-01-31 00:01:10 +0100

    fix : return http code error on http request exception

--- src/Middleware.API/Exceptions/Handlers/ProblemExceptionHandler.cs
+++ src/Middleware.API/Exceptions/Handlers/ProblemExceptionHandler.cs
@@ -5,26 +5,40 @@ using Newtonsoft.Json;
 namespace Middleware.API.Exceptions.Handlers;
 
 internal class ProblemExceptionHandler(
-    IProblemDetailsService problemDetailsService, 
+    IProblemDetailsService problemDetailsService,
     ILogger<ProblemExceptionHandler> logger
-    ) : IExceptionHandler
+) : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
     {
-        if (exception is not ProblemException problemException)
+        ProblemDetails problemDetails;
+
+        if (exception is HttpRequestException httpRequestException) {
+            problemDetails = new ProblemDetails()
+            {
+                Status = httpRequestException.StatusCode is not null ? (int)httpRequestException.StatusCode : StatusCodes.Status400BadRequest,
+                Title = httpRequestException.Message,
+                Detail = httpRequestException.InnerException?.Message,
+                Type = "Http Request Exception",
+            };
+        }
+        else if(exception is ProblemException problemException) {
+            problemDetails = new ProblemDetails()
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = problemException.Error,
+                Detail = problemException.Message,
+                Type = "Bad Request",
+            };
+        }
+        else {
             return true;
+        }
 
-        ProblemDetails problemDetails = new ProblemDetails()
-        {
-            Status = StatusCodes.Status400BadRequest,
-            Title = problemException.Error,
-            Detail = problemException.Message,
-            Type = "Bad Request",
-        };
-        
         logger.LogError(JsonConvert.SerializeObject(problemDetails));
-
         httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
         return await problemDetailsService.TryWriteAsync(
             new ProblemDetailsContext()
             {
